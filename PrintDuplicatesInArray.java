167ab139c8334e29bc64410511c20a60523884d5
thevikramr <132732496+thevikramr@users.noreply.github.com>
2023-05-17 21:44:51 +0530

    Add files via upload

---
+++ PrintDuplicatesInArray.java
@@ -0,0 +1,16 @@
+package week3.day1;
+
+public class PrintDuplicatesInArray {
+	public static void main(String[] args) {
+		int[] arr= {14,12,13,11,15,14,18,16,17,19,18,17,20};
+		for (int i = 0; i < arr.length; i++) {
+			for (int j =i+1; j < arr.length; j++) {
+				if (arr[i]==arr[j]) {
+					System.out.println(arr[i]);
+				}
+			}
+		}
+	}
+
+}
+
---
+++ SecondLargestNum.java
@@ -0,0 +1,11 @@
+package week3.day1;
+
+public class SecondLargestNum {
+	public static void main(String[] args) {
+
+		int[] num= {1,2,3,4,6,8,9,12};
+		int len=num.length;
+		System.out.println(num[len-2]);
+
+	}
+}
